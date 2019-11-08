f364cde6cc2864600ee8639f0778a58aa4b56ed5
yoz8ik <seminsergv@gmail.com>
2019-11-08 15:13:48 +0100

    function removed, while loop is added

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,20 +1,22 @@
 files_number=$(ls -1 | wc -l)
+count=1
 
-
-echo "Please, guess an amount of file in the current directory"
-read response
-
-function check_reply {  
-  echo "You have entered that there are $response files in here"
+while [[ $count -eq 1 ]]
+do
+  echo "Please, guess an amount of file in the current directory"
+  read response
   if  [[ $response -eq $files_number ]]
   then
     echo "Congratualtions! You are right! $response is correct value"
+    count=0
   elif [[ $response -gt $files_number ]]
   then
     echo "Your guess is too high. Please, try again."
+    count=1
   else
     echo "Your guess is too low. Please, try again."
+    count=1
   fi
-}
+done
 
-check_reply
+
